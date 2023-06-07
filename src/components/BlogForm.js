f2444e1aee06b843f6da8686f4b28242dbfba7b7
Jay Lovinsky <104098730+Jaysavvy@users.noreply.github.com>
2023-06-07 12:37:34 -0400

    Minor changes to frontend

--- src/components/BlogForm.js
+++ src/components/BlogForm.js
@@ -64,7 +64,7 @@ const BlogForm = (props) => {
       </div>
 
       <div>
-        <h2>contents</h2>
+        <h2>Blog contents</h2>
         {
           blogs.map(blog =>
             <Blog key={blog.id} blog={blog}
